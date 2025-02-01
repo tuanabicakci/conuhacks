b840ba062dedcb23e2fec35603d7d38d294adf55
tuanabicakci <bicakcituana@gmail.com>
2025-02-01 12:48:21 -0500

    added title

--- script.py
+++ script.py
@@ -2,6 +2,8 @@ import streamlit as st
 import numpy as np
 import pandas as pd
 
+
+st.title('Immigration stuff in MTL')
 map_data = pd.DataFrame(
     np.random.randn(1000, 2) / [50, 50] + [37.76, -122.4],
     columns=['lat', 'lon'])
